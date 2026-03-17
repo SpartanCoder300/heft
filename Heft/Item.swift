0f641d59580836919e31c314912953fa0ac6c313
SpartanCoder300 <150220925+SpartanCoder300@users.noreply.github.com>
2026-03-17 12:41:41 -0600

    Initial Commit

---
+++ Heft/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Heft
+//
+//  Created by Garrett Spencer on 3/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HeftTests/HeftTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeftTests.swift
+//  HeftTests
+//
+//  Created by Garrett Spencer on 3/17/26.
+//
+
+import Testing
+@testable import Heft
+
+struct HeftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
